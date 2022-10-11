ac9a23587684cbaea5c2b55e06169a97b1c02a8b
ponomarev <ponomarev@radioavionica.ru>
2022-10-11 19:32:57 +0300

    Add structure, which will help to change structe in the future

--- smart_pointer.cpp
+++ smart_pointer.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+struct A
+{
+    A(int a=0): a_(a){}
+    int get() const
+    {
+         return a_;
+    }
+
+    void print() const
+    {
+       std::cout<<"a = "<<a_<<std::endl;
+    }
+
+    void set(int a)
+    {
+        a_ = a;
+    }
+    
+
+private:
+    int a_;    
+};
+
+
 int main()
 {
-    std::cout<<"Hi"<<std::endl;
+    A a;
+    a.print();
+    std::cout<<"Pump it"<<std::endl;
     return 0;
 }
