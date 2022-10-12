c185103fb88c883018f92d6e1c2547047ef420f3
ponomarev <ponomarev@radioavionica.ru>
2022-10-12 17:53:14 +0300

    Made a scoped pointer

--- smart_pointer.cpp
+++ smart_pointer.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
-struct A
+using namespace std;
+
+struct AA
 {
-    A(int a=0): a_(a){}
+    AA(int a=0): a_(a){}
     int get() const
     {
          return a_;
@@ -13,21 +15,76 @@ struct A
        std::cout<<"a = "<<a_<<std::endl;
     }
 
-    void set(int a)
+    void set(int a){a_ = a;}
+
+    std::ostream& operator<< (std::ostream& out)
     {
-        a_ = a;
+        return out<<a_;
     }
-    
 
 private:
     int a_;    
 };
 
+std::ostream& operator<< (std::ostream& out, const AA& a)
+{
+    return out<<a.get();
+}
+
+
+struct ScopedPtr
+{
+    explicit 
+    ScopedPtr(AA* ptr = nullptr) : ptr_(ptr){};
+    ~ScopedPtr()
+    {
+        delete ptr_;
+    }
+
+    AA* get() const
+    {
+        return ptr_;
+    }
+
+    void reset(AA* ptr)
+    {
+        delete ptr_;
+        ptr_ = ptr;
+    }
+
+    AA* release()
+    {
+        AA* ptr = ptr_;
+        ptr_ = nullptr;
+        return ptr;
+    }
+
+    AA& operator*() const
+    {
+        return *ptr_;
+    }
+
+    AA* operator->()const
+    {
+        return ptr_;
+    }
+    private:
+    ScopedPtr(const ScopedPtr& ptr);
+    //ScopedPtr& operator=(const ScopedPtr& ptr);
+    AA* ptr_;
+};
+
 
 int main()
 {
-    A a;
-    a.print();
-    std::cout<<"Pump it"<<std::endl;
+    ScopedPtr p(new AA(12));
+    ScopedPtr p1;
+    p1 = p;
+    cout<<*(p.release())<<endl;
+    cout<<*(p.release())<<endl;
+
+    cout<<*(p1.release())<<endl;
+    //A a(12);
+    //std::cout<<a;
     return 0;
 }
